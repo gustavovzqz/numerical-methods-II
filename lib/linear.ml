c7d34e790a9190d4c5a91276760ceecd4ed9c432
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2024-06-07 18:27:27 -0300

    feat: lu decomposition

--- lib/linear.ml
+++ lib/linear.ml
@@ -1,11 +1,6 @@
 type vector = float array
 type matrix = float array array
 
-let zero_vector n = Array.make n 0.
-
-(* TODO: Will it always be a square matrix? *)
-let zero_matrix n = Array.init n (fun _ -> zero_vector n)
-
 let add_vectors v1 v2 =
   let n1 = Array.length v1 and n2 = Array.length v2 in
   assert (n1 = n2);
@@ -14,6 +9,9 @@ let add_vectors v1 v2 =
 let scale_vector k vec =
   let n = Array.length vec in
   Array.init n (fun i -> k *. vec.(i))
+
+let identity_matrix n =
+  Array.init n (fun i -> Array.init n (fun j -> if i <> j then 0. else 1.))
 
 let vector_norm vec =
   let sum_of_squares = Array.fold_left (fun acc x -> acc +. (x *. x)) 0. vec in
@@ -60,5 +58,22 @@ let regular_power_iteration mat vec epsilon =
     if error > epsilon then power_iteration new_lambda new_vec_k
     else (new_lambda, old_x1)
   in
-
   power_iteration 0. vec
+
+let lu_decomposition mat =
+  let n = Array.length mat and u = Array.copy mat in
+  let l = identity_matrix n in
+
+  (* For each line ...*)
+  for i = 0 to n - 1 do
+    (* For each column ... *)
+    for j = i + 1 to n - 1 do
+      (* Find the scale factor to the line and store in the L matrix*)
+      l.(j).(i) <- u.(j).(i) /. u.(i).(i);
+      for k = i to n - 1 do
+        (* Update the line using the scale factor *)
+        u.(j).(k) <- u.(j).(k) -. (l.(j).(i) *. u.(i).(k))
+      done
+    done
+  done;
+  (l, u)
