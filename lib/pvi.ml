94872941b2b6d5d45e3cf8e909b6c13e053bf2d7
= <gustavovzqz.dev@gmail.com>
2024-07-09 10:26:17 -0300

    wip

--- lib/pvi.ml
+++ lib/pvi.ml
@@ -1,17 +1,33 @@
-type state = float * float 
-type state_derivative = state -> float -> state 
+type state = float * float
+type state_derivative = state -> float -> state
 
-let init_state f1 f2 = f1 * f2 
+let init_state f1 f2 = f1 * f2
+let add_state (a0, a1) (b0, b1) = (a0 +. b0, a1 +. b1)
+let scale_state t (a0, a1) = (t *. a0, t *. a1)
 
-let derivative_of_state state_ expr t1 = 
-  let (v0, _) = state_ in 
+let derivative_of_state state_ expr t1 =
+  let v0, _ = state_ in
   (expr state_ t1, v0)
 
-let init_derivative state_ = derivative_of_state state_ 
+let init_derivative state_ = derivative_of_state state_
 
-let runge_kutta state_ t_i derivative dt = 
-  let f1 = derivative state_ t_i in 
-  let s_one_half = add state_ (scale_state (dt /. 2.) f1) in 
-  let f2 = derivative s_one_half (t_i +. (dt /. 2.)) in 
-  let s_aprox = state_ 
-  (* da pra seguir assim, depois tornar genérico *) 
+let runge_kutta state_ t_i derivative dt =
+  let f1 = derivative state_ t_i in
+  let half_dt = dt /. 2. in
+  let s_one_half = add_state state_ (scale_state half_dt f1) in
+  let f2 = derivative s_one_half (t_i +. half_dt) in
+
+  let minus_f1 = scale_state (-1.) f1 in
+  let double_one_half = scale_state 2. s_one_half in
+  let s_approx =
+    add_state state_ (scale_state dt (add_state minus_f1 double_one_half))
+  in
+  let f3 = derivative s_approx (t_i +. dt) in
+
+  let f1_aux = scale_state (1. /. 6.) f1
+  and f2_aux = scale_state (4. /. 6.) f2
+  and f3_aux = scale_state (1. /. 6.) f3 in
+
+  let aux_sum = add_state f1_aux (add_state f2_aux f3_aux) in
+
+  add_state state_ (scale_state dt aux_sum)
