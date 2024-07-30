21c6277b2973f6c8238a624de48e31f393306ae3
= <gustavovzqz.dev@gmail.com>
2024-07-29 22:01:59 -0300

    finishing

--- T13/main.ml
+++ T13/main.ml
@@ -7,5 +7,4 @@ let () =
   (* Expressão final que define a derivada *)
   let expr (v0, _) _ = -10. -. (0.25 /. 2. *. v0) in
   let deriv = Pvi.init_derivative expr in
-  Pvi.plot_function_fst initial_state 0. deriv 0.1 15.;
-  Pvi.plot_function_snd initial_state 0. deriv 0.1 15.
+  Pvi.plot_function_fst initial_state 0. deriv 0.1 15.
--- T14/main.ml
+++ T14/main.ml
@@ -1,1 +1,10 @@
-let () = ()
+open Numerical
+
+let () =
+  (* Velocidade inicial | Altura inicial *)
+  let initial_state = Pvi.init_state 5. 200. in
+
+  (* Expressão final que define a derivada *)
+  let expr (v0, _) _ = -10. -. (0.25 /. 2. *. v0) in
+  let deriv = Pvi.init_derivative expr in
+  Pvi.plot_function_snd_PC initial_state 0. deriv 0.1 15. 0.1
