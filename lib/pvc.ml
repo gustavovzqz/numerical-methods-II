e22cc62c6fc4cf84f2a74856547ec3a479ce7693
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2024-08-01 08:37:33 -0300

    wip

--- lib/pvc.ml
+++ lib/pvc.ml
@@ -7,11 +7,12 @@ let get_inverse_dx_squared nodes =
   let square x = x *. x in 
   square (1. /. (float_of_int nodes))
 
-  
+ 
 
-let finite_difference_M2 fun_xy bounds nodes = 
-  let nodes_matrix =  Mat.sequential nodes nodes and 
-  linear_system = Mat.zeros nodes nodes and 
+let finite_difference_M2 fun_xy bound nodes = 
+
+  (* TODO: Intialize matrix with bound values *)
+  let linear_system = Mat.zeros nodes nodes and 
   idx2 = get_inverse_dx_squared nodes in 
   let idy2 = idx2 in 
 
@@ -19,13 +20,15 @@ let finite_difference_M2 fun_xy bounds nodes =
   side_value = idx2 and 
   vertical_value = idy2 in 
 
+  (* i + 3 -> upper value *) 
+  (* i - 3 -> lower value *) 
+  (* i + 1 -> right value *) 
+  (* i - 1 -> left value  *) 
 
   let apply_mask r c = 
   for i = 0 to nodes do
-     
-
-
-
+    rv = 
+    Mat.set linear_system i (get_upper_bound
   done
 
 
