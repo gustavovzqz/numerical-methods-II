d4509ac8e4bedce3f4e43b1c0652e6104ac59bfd
= <gustavovzqz.dev@gmail.com>
2024-08-02 22:59:16 -0300

    despensando o que foi pensado pois estava errado

--- lib/pvc.ml
+++ lib/pvc.ml
@@ -4,54 +4,50 @@ let get_inverse_dx_squared nodes =
   let square x = x *. x in
   square (float_of_int nodes)
 
-(* TODO: Como saber se o elemento está na borda... não é tão simples... ou é? *)
-let init_matrix bound size =
-  Mat.init_2d size size (fun i j ->
-      match bound i j with Some number -> number | None -> 0.)
 
-let get_linear_system_M2 bound nodes =
-  let matrix_size = (nodes - 1) * (nodes - 1) in
-  (* TODO: Arrumar a função de inicialização (encontrar quem é a borda corretamente) e chamá-lá ao final do método *)
-  let linear_system = init_matrix bound matrix_size
-  and idx2 = get_inverse_dx_squared nodes in
-  let idy2 = idx2 in
+let get_side_values part =
 
-  let center_value = -2. *. (idx2 +. idy2)
+let idx2 = get_inverse_dx_squared part in 
+ let center_value = -2. *. (idx2 +. idx2)
   and side_value = idx2
-  and vertical_value = idy2 in
+  and vertical_value = idx2 in
+(center_value, side_value, vertical_value)
 
-  let validade_position pos = pos >= 0 && pos < matrix_size in
 
-  for i = 0 to matrix_size - 1 do
-    Printf.printf "Matriz da iteração %d\n" i;
-    Mat.print linear_system;
-    let rec set_pos_values val_list =
-      match val_list with
-      | (pos, value) :: t ->
-          if validade_position pos then (
-            Printf.printf "Setando na posição (%d, %d) o valor %f\n" i pos value;
-            Mat.set linear_system i pos value);
-          set_pos_values t
-      | _ -> ()
-    in
+let init_matrix rows cols f =
+  Array.init rows (fun i ->
+    Array.init cols (fun j -> f i j))
 
-    let upper_pos = i + nodes - 1
-    and lower_pos = i - nodes + 1
-    and right_pos = i + 1
-    and left_pos = i - 1
-    and center_pos = i in
+type node = 
+  | Bound of float 
+  | Equation 
 
-    let pos_val_list =
-      [
-        (upper_pos, vertical_value);
-        (lower_pos, vertical_value);
-        (right_pos, side_value);
-        (left_pos, side_value);
-        (center_pos, center_value);
-      ]
-    in
+let get_matrix_value i j ~bounds = 
+  let (lower, left, upper, right) = bounds in 
+  match (i, j) with 
+  | (_, 0) -> Bound lower
+  | (_, 1) -> Bound upper 
+  | (0, _) -> Bound left 
+  | (1, _) -> Bound right 
+  | _ -> Equation
 
-    set_pos_values pos_val_list
-  done;
+let get_linear_system_M2 bound partitions =
+  let nodes = partitions + 1 in 
+  let system_size = (partitions * partitions) in 
 
-  linear_system
+  let domain = init_matrix nodes nodes (get_matrix_value ~bounds:bound) and
+  linear_system = Mat.empty system_size system_size in 
+
+  let (center_value, side_value, vertical_value) = get_side_values partitions in 
+
+  let eq_pos = ref 0 in 
+  (* Domain = nodes x nodes, and we want to iter in the innner matrix, starting in [1][1] and going to [nodes - 2] [nodes - 2] *)
+  for i = 1 to nodes - 2  do
+    for j = 1 to nodes - 2 do
+      (* TODO: Make the linear system! Creating a match function to get the correct value *)
+    done
+  done
+
+  
+
+
